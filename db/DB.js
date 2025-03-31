3a23b6e467ce9ad7a86fec6a76d5386e5d157086
Matheesha Abiman <matheeshaabi@gmail.com>
2025-03-31 10:51:56 +0530

    Upload Complete POS

---
+++ db/DB.js
@@ -0,0 +1,188 @@
+export let Customers = [
+  {
+      custId: "C01",
+      custName: "Kavinu",
+      custAddress: "Galle",
+      custSalary: "100000",
+  },
+  {
+      custId: "C02",
+      custName: "Nimal",
+      custAddress: "Matara",
+      custSalary: "20000",
+  },
+  {
+      custId: "C03",
+      custName: "Sunil",
+      custAddress: "Colombo",
+      custSalary: "50000",
+  },
+  {
+      custId: "C04",
+      custName: "Kamala",
+      custAddress: "Kandy",
+      custSalary: "75000",
+  },
+  {
+      custId: "C05",
+      custName: "Saman",
+      custAddress: "Kurunegala",
+      custSalary: "45000",
+  },
+  {
+      custId: "C06",
+      custName: "Chathura",
+      custAddress: "Anuradhapura",
+      custSalary: "60000",
+  },
+  {
+      custId: "C07",
+      custName: "Mahesh",
+      custAddress: "Jaffna",
+      custSalary: "55000",
+  },
+  {
+      custId: "C08",
+      custName: "Roshan",
+      custAddress: "Negombo",
+      custSalary: "70000",
+  },
+  {
+      custId: "C09",
+      custName: "Harsha",
+      custAddress: "Rathnapura",
+      custSalary: "30000",
+  },
+  {
+      custId: "C10",
+      custName: "Ruwan",
+      custAddress: "Badulla",
+      custSalary: "65000",
+  },
+  {
+      custId: "C11",
+      custName: "Pradeep",
+      custAddress: "Puttalam",
+      custSalary: "40000",
+  },
+  {
+      custId: "C12",
+      custName: "Dilshan",
+      custAddress: "Hambantota",
+      custSalary: "48000",
+  },
+  {
+      custId: "C13",
+      custName: "Anura",
+      custAddress: "Trincomalee",
+      custSalary: "52000",
+  },
+  {
+      custId: "C14",
+      custName: "Sujeewa",
+      custAddress: "Batticaloa",
+      custSalary: "42000",
+  },
+  {
+      custId: "C15",
+      custName: "Lahiru",
+      custAddress: "Polonnaruwa",
+      custSalary: "57000",
+  }
+];
+  
+export let Items = [
+  {
+      itemId: "I01",
+      itemName: "Milk",
+      itemQty: 10,
+      itemPrice: 150,
+  },
+  {
+      itemId: "I02",
+      itemName: "Soya",
+      itemQty: 50,
+      itemPrice: 160,
+  },
+  {
+      itemId: "I03",
+      itemName: "Rice",
+      itemQty: 100,
+      itemPrice: 120,
+  },
+  {
+      itemId: "I04",
+      itemName: "Sugar",
+      itemQty: 40,
+      itemPrice: 180,
+  },
+  {
+      itemId: "I05",
+      itemName: "Tea",
+      itemQty: 30,
+      itemPrice: 250,
+  },
+  {
+      itemId: "I06",
+      itemName: "Bread",
+      itemQty: 25,
+      itemPrice: 80,
+  },
+  {
+      itemId: "I07",
+      itemName: "Butter",
+      itemQty: 15,
+      itemPrice: 600,
+  },
+  {
+      itemId: "I08",
+      itemName: "Cheese",
+      itemQty: 20,
+      itemPrice: 750,
+  },
+  {
+      itemId: "I09",
+      itemName: "Eggs",
+      itemQty: 30,
+      itemPrice: 45,
+  },
+  {
+      itemId: "I10",
+      itemName: "Chicken",
+      itemQty: 10,
+      itemPrice: 1500,
+  },
+  {
+      itemId: "I11",
+      itemName: "Fish",
+      itemQty: 18,
+      itemPrice: 1200,
+  },
+  {
+      itemId: "I12",
+      itemName: "Salt",
+      itemQty: 60,
+      itemPrice: 80,
+  },
+  {
+      itemId: "I13",
+      itemName: "Coconut Oil",
+      itemQty: 35,
+      itemPrice: 450,
+  },
+  {
+      itemId: "I14",
+      itemName: "Vegetables",
+      itemQty: 90,
+      itemPrice: 200,
+  },
+  {
+      itemId: "I15",
+      itemName: "Fruits",
+      itemQty: 75,
+      itemPrice: 300,
+  }
+];
+
+  export let Orders = [];
+  
